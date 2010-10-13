c2e39e490e69ab2cd75e57bde899db933f212059
nx111 <gdzdz@163.com>
2010-10-13 12:15:19 +0800

    20101013-svn3497

---
+++ build_sh4/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export OLDPATH=$PATH
+export PATH=../../toolchains/sh4/bin:$OLDPATH     # 指定编译源码时要用的PowerPC环境下的GCC和C++编译器路径
+make clean
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/toolchain-sh4-qboxhd.cmake -DWEBIF=1 ..    #用cmake命令对源码进行交叉编译
+make
+export PATH=$OLDPATH
