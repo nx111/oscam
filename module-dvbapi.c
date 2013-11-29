nx111/oscam

commit 2303306ab12fd8ec3b01c00b17ace9a098fa37d1

dvbapi: handle LIST_FIRST, LIST_MORE and LIST_LAST not only in pmt_mode=6
Above CA list management requests was only properly handled in
pmtmode=6, while it could be handled in other pmt modes as well.
As a result one single socket can be used to manage PMTs for
several services (eg. in pmt_mode=4)


git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9049 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-dvbapi.c
@@ -2638,13 +2638,8 @@ void dvbapi_handlesockmsg(unsigned char *buffer, uint32_t len, int32_t connfd)
 			break;
 		}
 
-		if(k > 0 && cfg.dvbapi_pmtmode != 6)
-		{
-			cs_log("Unsupported capmt. Please report");
-			cs_dump(buffer, len, "capmt: (k=%d)", k);
-		}
-		if(k > 0 && cfg.dvbapi_pmtmode == 6)
-			{ cs_ddump_mask(D_DVBAPI, buffer + k, len - k, "[DVBAPI] Parsing next PMT object(s):"); }
+		if(k > 0)
+			cs_ddump_mask(D_DVBAPI, buffer + k, len - k, "[DVBAPI] Parsing next PMT object(s):");
 
 		if(buffer[3 + k] & 0x80)
 		{
@@ -3588,38 +3583,14 @@ static void *dvbapi_main_local(void *cli)
 				if(type[i] == 1 && pmthandling == 0)
 				{
 					pmthandling = 1; // pmthandling in progress!
-					if(pfd2[i].fd == listenfd && cfg.dvbapi_pmtmode == 6)
-					{
-						int32_t size = 0;
-						uint32_t pmtlen = 0;
-						do
-						{
-							size = recv(listenfd, (mbuf + pmtlen), (sizeof(mbuf) - pmtlen), MSG_DONTWAIT);
-							if(size > 0) { pmtlen += size; }
-						}
-						while(size > 0 && pmtlen < sizeof(mbuf));
-						if(pmtlen < 3)
-						{
-							cs_debug_mask(D_DVBAPI, "[DVBAPI] CA PMT server message too short!");
-							pmthandling = 0; // pmthandling done!
-							continue;
-						}
-						if(!(pmtlen < sizeof(mbuf)))
-						{
-							cs_log("[DVBAPI] ***** WARNING: PMT BUFFER OVERFLOW, PLEASE REPORT! ****** ");
-						}
-						cs_ddump_mask(D_DVBAPI, mbuf, pmtlen, "New PMT info from server (total size: %d)", pmtlen);
-						disable_pmt_files = 1;
-						dvbapi_handlesockmsg(mbuf, pmtlen, listenfd);
-						pmthandling = 0; // pmthandling done!
-						continue; // continue with other events!
-					}
+					connfd = -1;     // initially no socket to read from
 
-					if(cfg.dvbapi_pmtmode != 6)
+					if (pfd2[i].fd == listenfd)
 					{
-
-						if(pfd2[i].fd == listenfd)
-						{
+						if (cfg.dvbapi_pmtmode == 6) {
+							connfd = listenfd;
+							disable_pmt_files = 1;
+						} else {
 							clilen = sizeof(servaddr);
 							connfd = accept(listenfd, (struct sockaddr *)&servaddr, (socklen_t *)&clilen);
 							cs_debug_mask(D_DVBAPI, "new socket connection fd: %d", connfd);
@@ -3627,30 +3598,47 @@ static void *dvbapi_main_local(void *cli)
 							if(cfg.dvbapi_pmtmode == 3 || cfg.dvbapi_pmtmode == 0) { disable_pmt_files = 1; }
 
 							if(connfd <= 0)
-							{
 								cs_debug_mask(D_DVBAPI, "accept() returns error on fd event %d (errno=%d %s)", pfd2[i].revents, errno, strerror(errno));
-								pmthandling = 0; // pmthandling done!
-								continue;
+						}
+					}
+					else
+					{
+						cs_debug_mask(D_DVBAPI, "PMT Update on socket %d.", pfd2[i].fd);
+						connfd = pfd2[i].fd;
+					}
+
+					//reading and completing data from socket
+					if (connfd > 0) {
+						uint32_t pmtlen = 0;
+
+						int tries = 100;
+						do {
+							len = recv(connfd, mbuf + pmtlen, sizeof(mbuf) - pmtlen, MSG_DONTWAIT);
+							if (len > 0)
+								pmtlen += len;
+							else {
+								if (pmtlen > 0) //all data read
+									break;
+								else {          //wait for data become available and try again
+									cs_sleepms(20);
+									continue;
+								}
+							}
+						} while (pmtlen < sizeof(mbuf) && tries--);
+
+						if (pmtlen > 0) {
+							if (pmtlen < 3)
+								cs_debug_mask(D_DVBAPI, "[DVBAPI] CA PMT server message too short!");
+							else {
+								if (pmtlen >= sizeof(mbuf))
+									cs_log("[DVBAPI] ***** WARNING: PMT BUFFER OVERFLOW, PLEASE REPORT! ****** ");
+								cs_ddump_mask(D_DVBAPI, mbuf, pmtlen, "New PMT info from socket (total size: %d)", pmtlen);
+								dvbapi_handlesockmsg(mbuf, pmtlen, connfd);
 							}
 						}
-						else
-						{
-							cs_debug_mask(D_DVBAPI, "PMT Update on socket %d.", pfd2[i].fd);
-							connfd = pfd2[i].fd;
-						}
-
-						len = read(connfd, mbuf, sizeof(mbuf));
-
-						if(len < 3)
-						{
-							cs_debug_mask(D_DVBAPI, "camd.socket: too small message received");
-							pmthandling = 0; // pmthandling done!
-							continue; // no msg received continue with other events!
-						}
-						dvbapi_handlesockmsg(mbuf, len, connfd);
-						pmthandling = 0; // pmthandling done!
-						continue; // continue with other events!
 					}
+					pmthandling = 0; // pmthandling done!
+					continue; // continue with other events!
 				}
 				else     // type==0
 				{

commit 0e354d661d15b46e783ef4ba2af90c6d206e7b54

dvbapi: fix polling socket for data after r9049
When client connects and doesn't send data immediately, there
was no further read attempts.
Bug was introduced in r9049 after switch to non-blocking read.

Fixed with additional table for sockets which has to be polled
when not yet associated with demux.


git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9063 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-dvbapi.c
@@ -51,6 +51,7 @@ static int32_t selected_box = -1;
 static int32_t selected_api = -1;
 static int32_t dir_fd = -1;
 static int32_t ca_fd[8];
+static int32_t unassoc_fd[MAX_DEMUX];
 static LLIST *channel_cache;
 
 struct s_emm_filter
@@ -2489,6 +2490,11 @@ int32_t dvbapi_parse_capmt(unsigned char *buffer, uint32_t length, int32_t connf
 	demux[demux_id].socket_fd = connfd;
 	demux[demux_id].stopdescramble = 0; // remove deletion mark!
 
+	// remove from unassoc_fd when necessary
+	for (j = 0; j < MAX_DEMUX; j++)
+		if (unassoc_fd[j] == connfd)
+			unassoc_fd[j] = 0;
+
 	char channame[32];
 	get_servicename(dvbapi_client, demux[demux_id].program_number, demux[demux_id].ECMpidcount > 0 ? demux[demux_id].ECMpids[0].CAID : 0, channame);
 	cs_log("New program number: %04X (%s) [pmt_list_management %d]", program_number, channame, ca_pmt_list_management);
@@ -3295,6 +3301,7 @@ static void *dvbapi_main_local(void *cli)
 
 	memset(demux, 0, sizeof(struct demux_s) * MAX_DEMUX);
 	memset(ca_fd, 0, sizeof(ca_fd));
+	memset(unassoc_fd, 0, sizeof(unassoc_fd));
 
 	dvbapi_read_priority();
 	dvbapi_detect_api();
@@ -3403,6 +3410,13 @@ static void *dvbapi_main_local(void *cli)
 
 		for(i = 0; i < MAX_DEMUX; i++)
 		{
+			// add client fd's which are not yet associated with the demux but needs to be polled for data
+			if (unassoc_fd[i]) {
+				pfd2[pfdcount].fd = unassoc_fd[i];
+				pfd2[pfdcount].events = (POLLIN | POLLPRI);
+				type[pfdcount++] = 1;
+			}
+
 			if(demux[i].program_number == 0) { continue; }  // only evalutate demuxers that have channels assigned
 
 			uint32_t ecmcounter = 0, emmcounter = 0;
@@ -3584,6 +3598,7 @@ static void *dvbapi_main_local(void *cli)
 				{
 					pmthandling = 1; // pmthandling in progress!
 					connfd = -1;     // initially no socket to read from
+					int new = 0;
 
 					if (pfd2[i].fd == listenfd)
 					{
@@ -3594,6 +3609,7 @@ static void *dvbapi_main_local(void *cli)
 							clilen = sizeof(servaddr);
 							connfd = accept(listenfd, (struct sockaddr *)&servaddr, (socklen_t *)&clilen);
 							cs_debug_mask(D_DVBAPI, "new socket connection fd: %d", connfd);
+							new = 1;
 
 							if(cfg.dvbapi_pmtmode == 3 || cfg.dvbapi_pmtmode == 0) { disable_pmt_files = 1; }
 
@@ -3620,11 +3636,29 @@ static void *dvbapi_main_local(void *cli)
 								if (pmtlen > 0) //all data read
 									break;
 								else {          //wait for data become available and try again
+
+									// remove from unassoc_fd if the socket fd is invalid
+									if (errno == EBADF)
+										for (j = 0; j < MAX_DEMUX; j++)
+											if (unassoc_fd[j] == connfd)
+												unassoc_fd[j] = 0;
 									cs_sleepms(20);
 									continue;
 								}
 							}
 						} while (pmtlen < sizeof(mbuf) && tries--);
+
+						// if the connection is new and we read no data, then add it to the poll,
+						// otherwise this socket will not be checked with poll when data arives
+						// because fd it is not yet assigned with the demux
+						if (new && !pmtlen) {
+							for (j = 0; j < MAX_DEMUX; j++) {
+								if (!unassoc_fd[j]) {
+									unassoc_fd[j] = connfd;
+									break;
+								}
+							}
+						}
 
 						if (pmtlen > 0) {
 							if (pmtlen < 3)