nx111/oscam

commit b22a1a644015b0cfdcba8e47137fb3449f547ce8

dvbapi: don't close socket in network mode on demux stop
References r9906


git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9921 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-dvbapi.c
@@ -2905,20 +2905,23 @@ void dvbapi_handlesockmsg(unsigned char *buffer, uint32_t len, int32_t connfd)
 						break;
 					}
 				}
-				// check do we have any demux running on this fd
-				int16_t execlose = 1;
-				for(i = 0; i < MAX_DEMUX; i++)
+				if (cfg.dvbapi_boxtype == BOXTYPE_IPBOX)
 				{
-					if(demux[i].socket_fd == connfd)
+					// check do we have any demux running on this fd
+					int16_t execlose = 1;
+					for(i = 0; i < MAX_DEMUX; i++)
 					{
-						execlose = 0;
-						break;
+						if(demux[i].socket_fd == connfd)
+						{
+							execlose = 0;
+							break;
+						}
 					}
-				}
-				if(execlose)
-				{
-					int32_t ret = close(connfd);
-					if(ret < 0) { cs_log("ERROR: Could not close PMT fd (errno=%d %s)", errno, strerror(errno)); }
+					if(execlose)
+					{
+						int32_t ret = close(connfd);
+						if(ret < 0) { cs_log("ERROR: Could not close PMT fd (errno=%d %s)", errno, strerror(errno)); }
+					}
 				}
 			}
 			else

commit 0e5762cac9e8ab017c036f3d7f4f3bd7e33338b0

dvbapi: fix obtaining duplicate PMT
This was broken in r9921

Closes trac issue #3923


git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9929 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-dvbapi.c
@@ -4027,9 +4027,8 @@ static void *dvbapi_main_local(void *cli)
 									{
 										cs_ddump_mask(D_DVBAPI, mbuf, chunksize, "[DVBAPI] Parsing #%d PMT object(s):", chunks_processed);
 										dvbapi_handlesockmsg(mbuf, chunksize, connfd);
-										if ((opcode & 0xFFFFFF00) == DVBAPI_AOT_CA_PMT)
-											add_to_poll = 0;
-										else if (opcode == DVBAPI_AOT_CA_STOP)
+										add_to_poll = 0;
+										if (cfg.dvbapi_listenport && opcode == DVBAPI_AOT_CA_STOP)
 											add_to_poll = 1;
 									}
 									else switch (opcode)