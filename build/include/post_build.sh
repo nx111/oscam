53dae0de832c3764c9ec28d99a8f497181c8a9e1
nx111 <gd.zhangdz@gmail.com>
2016-09-15 14:38:08 +0800

    build scripts: fix sed error.

--- build/include/post_build.sh
+++ build/include/post_build.sh
@@ -39,11 +39,13 @@ if [ -f $ROOT/build/.tmp/image${machine}/${OSCAM_TARGET} ]; then
 		target_size=$(( total_size - control_size ))
 		sed -e "s/^\([[:space:]]*Version:\).*/\1 ${revision}/" -e "s/^\([[:space:]]*Installed-Size:\).*/\1 ${target_size}/" \
 		    -e "s/^\([[:space:]]*Architecture:\).*/\1 ${plat}/" -i ${builddir}/image${machine}/CONTROL/control
-		config_file=$(find  "$ROOT/build/.tmp/image${machine}" -name "oscam.conf*" -type f | sed -n 1p -e "s:$ROOT/build/.tmp/image${machine}/::")
+		config_file=$(find  "$ROOT/build/.tmp/image${machine}" -name "oscam.conf*" -type f | \
+			             sed -n 1p | sed -e "s:$ROOT/build/.tmp/image${machine}/::")
 		if [ "${config_file}" != "" ]; then
 			config_dir=$(dirname ${config_file});
 			[ -f $ROOT/build/.tmp/SoftCam.Key ] && cp $ROOT/build/.tmp/SoftCam.Key $ROOT/build/.tmp/image${machine}/${config_dir}/
-			find "$ROOT/build/.tmp/image${machine}/${config_dir}" -type f | sed -e "s:$ROOT/build/.tmp/image${machine}::" > $ROOT/build/.tmp/image${machine}/CONTROL/conffiles
+			find "$ROOT/build/.tmp/image${machine}/${config_dir}" -type f | \
+			      sed -e "s:$ROOT/build/.tmp/image${machine}::" > $ROOT/build/.tmp/image${machine}/CONTROL/conffiles
 		fi
 		$(dirname ${builddir})/include/ipkg-build -c $ROOT/build/.tmp/image${machine} ${builddir} > /dev/null
 		ipkfile=$(find $builddir -name *.ipk |xargs basename)
