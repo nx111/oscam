9dbd4f7b983c5c3eb95a8d865da7c1f170aab5d1
nx111 <gd.zhangdz@gmail.com>
2016-09-18 13:00:58 +0800

    fix check_sec_len for some large ecm data.

--- reader-common.c
+++ reader-common.c
@@ -18,12 +18,12 @@
 extern const struct s_cardsystem *cardsystems[];
 extern char *RDR_CD_TXT[];
 
-int32_t check_sct_len(const uchar *data, int32_t off)
+int32_t check_sct_len(const uchar *data, int32_t off, size_t maxSize)
 {
-	int32_t len = SCT_LEN(data);
-	if(len + off > MAX_LEN)
+	size_t len = SCT_LEN(data);
+	if(len + off > maxSize)
 	{
-		cs_log_dbg(D_TRACE | D_READER, "check_sct_len(): smartcard section too long %d > %d", len, MAX_LEN - off);
+		cs_log_dbg(D_TRACE | D_READER, "check_sct_len(): smartcard section too long %d > %d", len, maxSize - off);
 		len = -1;
 	}
 	return len;
