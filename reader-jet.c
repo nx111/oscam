a2881aa22d30b06f65e9e99b98cd1b4e262ca53e
nx111 <gd.zhangdz@gmail.com>
2016-10-19 16:55:39 +0800

    jet: try to simplify write cmd.

--- reader-jet.c
+++ reader-jet.c
@@ -7,41 +7,63 @@
 
 #define CRC16 0x8005
 
-uint16_t calc_crc16(const uint8_t *data, uint16_t size)
-{
-    uint16_t out = 0;
-    int bits_read = 0, bit_flag;
 
-    /* Sanity check: */
-    if(data == NULL)
-        return 0;
-
-    while(size > 0)
-    {
-        bit_flag = out >> 15;
-
-        /* Get next bit: */
-        out <<= 1;
-        out |= (*data >> (7 - bits_read)) & 1;
-
-        /* Increment bit counter: */
-        bits_read++;
-        if(bits_read > 7)
-        {
-            bits_read = 0;
-            data++;
-            size--;
-        }
-
-        /* Cycle check: */
-        if(bit_flag)
-            out ^= CRC16;
-
-    }
-    return out;
+#define int jet_exec_cmd(cmd, len, encrypt_tag, title)\
+{\
+	uint8_t cmd_buf[256];\
+	uint8_t cmd_tmp[256];\
+	memset(cmd_buf, 0, sizeof(cmd_buf));\
+	memcpy(cmd_buf, cmd, len);\
+	uint16_t crc=calc_crc16(cmd_buf, len);\
+	cmd_buf[len] = crc >> 8;\
+	cmd_buf[len + 1] = crc & 0xFF;\
+	if(!jet_encrypt(encrypt_tag, cmd_buf, len + 2, cmd_tmp, sizeof(cmd_tmp))){\
+		rdr_log(reader, "error: encrypt %s failed.", title);\
+		return ERROR;\
+	}\
+	cmd_tmp[4] += 2;\
+	write_cmd(cmd_tmp, cmd_tmp + 5);\
+	if(cta_res[cta_lr - 2] != 0x90 || cta_res[cta_lr - 1] != 0x00){\
+		rdr_log(reader, "error: exec %s failed!", title);\
+		return ERROR;\
+	}\
 }
 
-static size_t encrypt(uint8_t tag, char * data, size_t len, char *output, size_t maxlen)
+static uint16_t calc_crc16( const uint8_t *data, size_t size)
+{
+	uint16_t out = 0;
+	int bits_read = 0, bit_flag;
+
+	/* Sanity check: */
+	if(data == NULL)
+		return 0;
+
+	while(size > 0)
+	{
+		bit_flag = out >> 15;
+
+		/* Get next bit: */
+		out <<= 1;
+		out |= (*data >> (7 - bits_read)) & 1;
+
+		/* Increment bit counter: */
+		bits_read++;
+		if(bits_read > 7)
+		{
+			bits_read = 0;
+			data++;
+			size--;
+		}
+
+		/* Cycle check: */
+		if(bit_flag)
+			out ^= CRC16;
+
+	}
+	return out;
+}
+
+static size_t jet_encrypt(uint8_t tag, uint8_t * data, size_t len, uint8_t *output, size_t maxlen)
 {
 	uint8_t buf[256];
 	size_t aligned_len = (len + 15) / 16 * 16;
@@ -116,9 +138,9 @@ static int32_t jet_card_init(struct s_reader *reader, ATR *newatr)
 	uint8_t get_serial_cmd01[37] = {0x21, 0x21, 0x00, 0x00, 0x00};
 	uint8_t get_rootkey_cmd[6] = {0x58, 0x02, 0x00, 0x00, 0x00, 0x00};
 	uint8_t get_authkey_cmd[6] = {0x58, 0x02, 0x00, 0x00, 0x00, 0x00};
-	def_resp;
 	get_atr;
-	uint8_t cmd[256];
+	def_resp;
+	//uint8_t cmd[256];
 	uint8_t cmd_buf[256];
 	uint8_t temp[256];
 	size_t cmdLen;
@@ -135,39 +157,11 @@ static int32_t jet_card_init(struct s_reader *reader, ATR *newatr)
 	memset(reader->prid, 0x00, sizeof(reader->prid));
 
 	// get serial step1
-	memset(cmd_buf, 0, sizeof(cmd_buf));
-	memcpy(cmd_buf, get_serial_cmd01, sizeof(get_serial_cmd01));
-	crc=calc_crc16(0, cmd_buf, sizeof(get_serial_cmd01));
-	cmd_buf[sizeof(get_serial_cmd01)] = crc >> 8;
-	cmd_buf[sizeof(get_serial_cmd01) + 1] = crc & 0xFF;
-	if(!encrypt(0xAA, cmd_buf, sizeof(get_serial_cmd01) + 2, cmd, sizeof(cmd))){
-		rdr_log(reader, "error: encrypt get_serial_cmd01 failed.");
-		return ERROR;
-	}
-	cmd[4] += 2;
-	write_cmd(cmd, cmd + 5);
-	if(cta_res[cta_lr - 2] != 0x90 || cta_res[cta_lr - 1] != 0x00){
-		rdr_log(reader, "error: get serial step 1 failed!");
-		return ERROR;
-	}
+	if(ERROR == jet_exec_cmd(get_serial_cmd01, sizeof(get_serial_cmd01), 0xAA, "get_serial_cmd01")) return ERROR;
 	memcpy(reader->hexserial, cta_res + 9, 8);
 
 	//get root key
-	memset(cmd_buf, 0, sizeof(cmd_buf));
-	memcpy(cmd_buf, get_rootkey_cmd, sizeof(get_rootkey_cmd);
-	crc=calc_crc16(0, cmd_buf, sizeof(get_rootkey_cmd));
-	cmd_buf[sizeof(get_rootkey_cmd)] = crc >> 8;
-	cmd_buf[sizeof(get_rootkey_cmd) + 1] = crc & 0xFF;
-	if (!encrypt(0xAA, cmd_buf, sizeof(get_rootkey_cmd) + 2, cmd, sizeof(cmd))){
-		rdr_log(reader, "error: encrypt get_rootkey_cmd failed.");
-		return ERROR;
-	}
-	cmd[4] += 2;
-	write_cmd(cmd, cmd + 5);
-	if(cta_res[cta_lr - 2] != 0x90 || cta_res[cta_lr - 1] != 0x00){
-		rdr_log(reader, "error: get root key failed!");
-		return ERROR;
-	}
+	if(ERROR == jet_exec_cmd(get_rootkey_cmd, sizeof(get_rootkey_cmd), 0xAA, "get_rootkey_cmd")) return ERROR;
 	memcpy(temp, cta_res + 5, cta_res[4]);
 	aes_twofish_decrypt(temp, cta_res[4]);
 	memset(reader->jet_root_key, 0 ,sizeof(reader->jet_root_key));
@@ -179,35 +173,15 @@ static int32_t jet_card_init(struct s_reader *reader, ATR *newatr)
 		rdr_log(reader, "error: generate derivekey faild, buffer overflow!");
 		return ERROR;
 	}
-	if(!encrypt(0xAA, cmd_buf, sizeof(reader->jet_derive_key), cmd, sizeof(cmd))){
-		rdr_log(reader, "error: encrypt for getting derive key failed.");
-		return ERROR;
-	}
-	cmd[4] += 2;
-	write(cmd, cmd + 5);
-	if(cta_res[cta_lr - 2] != 0x90 || cta_res[cta_lr - 1] != 0x00){
-		rdr_log(reader, "error: confirm derive key failed!");
-		return ERROR;
-	}
+	//generate_derivekey has filled crc16. so call jet_exec_cmd with len - 2.
+	if(ERROR == jet_exec_cmd(cmd_buf, sizeof(reader->jet_derive_key) - 2, 0xAA, "get derivekey cmd")) return ERROR;
 	memcpy(reader->jet_derive_key, cmd_buf, sizeof(reader->jet_derive_key));
 
 	//get auth key
 	memset(cmd_buf, 0, sizeof(cmd_buf));
-	memcpy(cmd_buf, get_authkey_cmd, sizeof(get_authkey_cmd);
-	memcpy(cmd_buf + 4, reader->jet_root_key, 2));
-	crc=calc_crc16(0, cmd_buf, sizeof(get_authkey_cmd));
-	cmd_buf[sizeof(get_authkey_cmd)] = crc >> 8;
-	cmd_buf[sizeof(get_authkey_cmd) + 1] = crc & 0xFF;
-	if (!encrypt(0xAA, cmd_buf, sizeof(get_rootkey_cmd) + 2, cmd, sizeof(cmd))){
-		rdr_log(reader, "error: encrypt get_authkey_cmd failed.");
-		return ERROR;
-	}
-	cmd[4] += 2;
-	write_cmd(cmd, cmd + 5);
-	if(cta_res[cta_lr - 2] != 0x90 || cta_res[cta_lr - 1] != 0x00){
-		rdr_log(reader, "error: get auth key failed!");
-		return ERROR;
-	}
+	memcpy(cmd_buf, get_authkey_cmd, sizeof(get_authkey_cmd));
+	memcpy(cmd_buf + 4, reader->jet_root_key, 2);
+	if(ERROR == jet_exec_cmd(cmd_buf, sizeof(get_authkey_cmd), 0xAA, "get_auth_cmd"))return ERROR;
 	memset(temp, 0, sizeof(temp));
 	memcpy(temp, cta_res, cta_res[4]);
 	des_twofish_decrypt(temp, cta_res[4]);
