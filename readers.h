a93df8896a7242a6c1ae5ee8acc96d9d32f0e0fb
nx111 <gd.zhangdz@gmail.com>
2016-10-19 16:53:56 +0800

    jet: add lost declear in readers.h

--- readers.h
+++ readers.h
@@ -18,4 +18,5 @@ extern const struct s_cardsystem reader_griffin;
 extern const struct s_cardsystem reader_dgcrypt;
 extern const struct s_cardsystem reader_streamguard;
 extern const struct s_cardsystem reader_emu;
+extern const struct s_cardsystem reader_jet;
 #endif
