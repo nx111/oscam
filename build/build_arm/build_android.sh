8242b951558b856ccf38d9de125cae22cdea5902
nx111 <gd.zhangdz@gmail.com>
2016-12-03 21:29:45 +0800

    build scripts: update for build android arm version.

--- build/build_arm/build_android.sh
+++ build/build_arm/build_android.sh
@@ -14,10 +14,22 @@ builddir=`cd $(dirname $0);pwd`
 . $(dirname $builddir)/include/pre_build.sh
 
 ##############################################
-PATH=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/bin:$PATH \
-   cmake  -DCMAKE_TOOLCHAIN_FILE=$ROOT/toolchains/${TOOCHAINFILE} $ROOT
-make LIB_PTHREAD=  ANDROID_NDK=1 \
-     CFLAGS=-I${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include
+   PATH=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/bin:$PATH \
+   cmake  -DCMAKE_TOOLCHAIN_FILE=$ROOT/toolchains/${TOOCHAINFILE}\
+	  -DOPTIONAL_INCLUDE_DIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include\
+	  -DOPENSSL_INCLUDE_DIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include\
+	  -DLIBUSBDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
+	  -DLIBRTDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
+	  -DLIBPCSCDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
+	  -DWITH_SSL=0\
+	  -DSTATIC_LIBUSB=1\
+	  -DSTATIC_LIBPCSC=1\
+	  --clean-first\
+	  -DWEBIF=1 $ROOT
+   feature=-pcsc-inline
+
+make STAGING_DIR=${TOOLCHAIN_STAGE} LIB_PTHREAD=  ANDROID_NDK=1 \
+     CFLAGS="-I${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include"
 
 ##############################################
 
