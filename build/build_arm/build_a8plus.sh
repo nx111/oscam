d8395cd50e9a7cccad4e5af05307e1a224f52ed4
nx111 <gd.zhangdz@gmail.com>
2016-12-15 14:04:18 +0800

    a8plus: update build script and libA8PlusXCam.a

--- build/build_arm/build_a8plus.sh
+++ build/build_arm/build_a8plus.sh
@@ -16,6 +16,7 @@ eval "sed -e \"s/\(.*OSCAM_SYSTEM_NAME \).*)/\1A8Plus)/\" -i $ROOT/toolchains/${
 
 ##############################################
    mv $ROOT/module-dvbapi-azbox.c $ROOT/module-dvbapi-azbox.c.orig
+   mv $ROOT/csctapi/ifd_sci.c $ROOT/csctapi/ifd_sci.c.orig
 
    PATH=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/bin:$PATH \
    cmake  -DCMAKE_TOOLCHAIN_FILE=$ROOT/toolchains/${TOOCHAINFILE}\
@@ -23,13 +24,12 @@ eval "sed -e \"s/\(.*OSCAM_SYSTEM_NAME \).*)/\1A8Plus)/\" -i $ROOT/toolchains/${
 	  -DOPENSSL_INCLUDE_DIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include\
 	  -DLIBUSBDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
 	  -DLIBRTDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
-	  -DLIBPCSCDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
 	  -DLIBSSLDIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
 	  -DLIBCRYPTODIR=${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr \
+	  -DHAVE_PCSC=0\
 	  -DWITH_SSL=0\
 	  -DSTATIC_LIBSSL=1\
 	  -DSTATIC_LIBUSB=1\
-	  -DSTATIC_LIBPCSC=1\
 	  --clean-first\
 	  -DWEBIF=1 $ROOT
    feature=-pcsc-inline
@@ -38,6 +38,7 @@ eval "sed -e \"s/\(.*OSCAM_SYSTEM_NAME \).*)/\1A8Plus)/\" -i $ROOT/toolchains/${
      CFLAGS="-I${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include:${TOOLCHAIN_STAGE}/${TOOLCHAIN_ROOT}/sysroot/usr/include/openssl"
 
    mv $ROOT/module-dvbapi-azbox.c.orig $ROOT/module-dvbapi-azbox.c
+   mv $ROOT/csctapi/ifd_sci.c.orig $ROOT/csctapi/ifd_sci.c
 
 
 ##############################################
